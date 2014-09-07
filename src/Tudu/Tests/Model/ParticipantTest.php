1049dd5c4c3e38815cd2a84b07dec6649146e460
Wouter Admiraal <wad@wadmiraal.net>
2014-09-07 19:02:24 +0200

    Add unit tests for participants.

---
+++ src/Tudu/Tests/Model/ParticipantTest.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * @file
+ * Todo model unit tests.
+ */
+
+namespace Tudu\Tests\Model;
+
+use Tudu\Tests\Model\AbstractModelTestClass;
+use Tudu\Model\Participant;
+
+class ParticipantTest extends AbstractModelTestClass
+{
+    /**
+     * Test the setters and getters.
+     */
+    public function testSettersAndGetters()
+    {
+        $participant = new Participant($this->getDBDriver());
+        $participant->setTodoID('sadpoi');
+        $this->assertEquals('sadpoi', $participant->getTodoID(), 'Test set/get todo ID.');
+        $participant->setEmail('email@email.com');
+        $this->assertEquals('email@email.com', $participant->getEmail(), 'Test set/get email.');
+        $participant->setName('John Doe');
+        $this->assertEquals('John Doe', $participant->getName(), 'Test set/get name.');
+        $participant->setLastMessageID('asd');
+        $this->assertEquals('asd', $participant->getLastMessageID(), 'Test set/get last message ID.');
+    }
+
+    /**
+     * Test persisting data to the database.
+     */
+    public function testDB()
+    {
+        // Test saving.
+        $participant = new Participant($this->connection);
+        $participant->setTodoID('ID');
+        $participant->setEmail('owner@email.com');
+        $participant->setName('John Doe');
+        $participant->save();
+
+        // Test loading.
+        $participant2 = new Participant($this->connection);
+        $stmt = $this->connection->prepare("
+            SELECT  *
+              FROM  participants
+             WHERE  todo_id = :id AND email = :email
+        ");
+        $stmt->bindValue('id', $participant->getTodoID());
+        $stmt->bindValue('email', $participant->getEmail());
+        $stmt->execute();
+        $rows = $stmt->fetchAll();
+        $participant2->loadFromDBRow($rows[0]);
+        $this->assertEquals($participant, $participant2, 'Loading loads the same attributes.');
+    }
+}
